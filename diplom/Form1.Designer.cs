331390f891fb96768dbb68ac083a8691ad70f563
Volosenkov Evgeniy <zhezldark2@mail.ru>
2016-04-09 19:06:06 +0400

    update

--- diplom/Form1.Designer.cs
+++ diplom/Form1.Designer.cs
@@ -28,50 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // simpleOpenGlControl1
+            // AnT
             // 
-            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
-            this.simpleOpenGlControl1.AutoCheckErrors = false;
-            this.simpleOpenGlControl1.AutoFinish = false;
-            this.simpleOpenGlControl1.AutoMakeCurrent = true;
-            this.simpleOpenGlControl1.AutoSwapBuffers = true;
-            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
-            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
-            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
-            this.simpleOpenGlControl1.Location = new System.Drawing.Point(5, 5);
-            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
-            this.simpleOpenGlControl1.Size = new System.Drawing.Size(438, 326);
-            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
-            this.simpleOpenGlControl1.TabIndex = 0;
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(5, 5);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(500, 500);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AnT_KeyDown_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "X",
+            "Y",
+            "Z"});
+            this.comboBox1.Location = new System.Drawing.Point(538, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Interval = 30;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(569, 35);
+            this.button1.Location = new System.Drawing.Point(578, 138);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(140, 26);
-            this.button1.TabIndex = 1;
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(598, 117);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(588, 200);
+            this.textBox1.Location = new System.Drawing.Point(553, 69);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
@@ -80,13 +93,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 340);
+            this.ClientSize = new System.Drawing.Size(865, 548);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.AnT);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,9 +108,10 @@
 
         #endregion
 
-        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer RenderTimer;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- diplom/Form1.cs
+++ diplom/Form1.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform;
 namespace diplom
 {
     public partial class Form1 : Form
@@ -14,6 +16,308 @@ namespace diplom
         public Form1()
         {
             InitializeComponent();
+            // инициализация для работы с openGL 
+            AnT.InitializeContexts();
         }
+
+        // массив вершин создаваемого геометрического объекта 
+        private float[,] GeomObject = new float[32, 3];
+        // счетчик его вершин 
+        private int count_elements = 0;
+
+        // событие загрузки формы окна 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // инициализация OpenGL, много раз комментированная ранее 
+            // инициализация библиотеки glut 
+            Glut.glutInit();
+            // инициализация режима экрана 
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+
+            // установка цвета очистки экрана (RGBA) 
+            Gl.glClearColor(255, 255, 255, 1);
+
+            // установка порта вывода 
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            // активация проекционной матрицы 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очистка матрицы 
+            Gl.glLoadIdentity();
+
+            Glu.gluPerspective(45, (float)AnT.Width / (float)AnT.Height, 0.1, 200);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+
+            // пирамида для визуализации (4 точки) 
+
+            GeomObject[0, 0] = -0.7f;
+            GeomObject[0, 1] = 0;
+            GeomObject[0, 2] = 0;
+
+            GeomObject[1, 0] = 0.7f;
+            GeomObject[1, 1] = 0;
+            GeomObject[1, 2] = 0;
+
+            GeomObject[2, 0] = 0.0f;
+            GeomObject[2, 1] = 0;
+            GeomObject[2, 2] = 1.0f;
+
+            GeomObject[3, 0] = 0;
+            GeomObject[3, 1] = 0.7f;
+            GeomObject[3, 2] = 0.3f;
+
+            // количество вершин рассматриваемого геометрического объекта 
+            count_elements = 4;
+            // устанавливаем ось X по умолчанию 
+            comboBox1.SelectedIndex = 0;
+
+            // начало визуализации (активируем таймер) 
+            RenderTimer.Start();
+
+        }
+
+        private void AnT_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void AnT_KeyDown_1(object sender, KeyEventArgs e)
+        {
+         /*   // Z и X отвечают за масштабирование 
+            if (e.KeyCode == Keys.Z)
+            {
+                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
+                CreateZoom(1.05f, comboBox1.SelectedIndex);
+            }
+            if (e.KeyCode == Keys.X)
+            {
+                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
+                CreateZoom(0.95f, comboBox1.SelectedIndex);
+            }
+
+            // W и S отвечают за перенос 
+            if (e.KeyCode == Keys.W)
+            {
+                // вызов функции, в которой мы реализуем перенос - передаем значение перемещения и выбранную ось в окне программы 
+                CreateTranslate(0.05f, comboBox1.SelectedIndex);
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                // вызов функции, в которой мы реализуем перенос - передаем значение перемещения и выбранную ось в окне программы 
+                CreateTranslate(-0.05f, comboBox1.SelectedIndex);
+
+            } // A и D отвечают за поворот 
+            if (e.KeyCode == Keys.A)
+            {
+                // вызов функции, в которой мы реализуем поворот - передаем значение для поворота и выбранную ось 
+                CreateRotate(0.05f, comboBox1.SelectedIndex);
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                CreateRotate(-0.05f, comboBox1.SelectedIndex);
+            }*/
+
+        }
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            // обработка "тика" таймера - вызов функции отрисовки 
+            Render();
+        }
+        private void Render()
+        {
+      /*      // очистка буфера цвета и буфера глубины 
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glClearColor(255, 255, 255, 1);
+            // очищение текущей матрицы 
+            Gl.glLoadIdentity();
+
+            // установка черного цвета 
+            Gl.glColor3f(0, 0, 0);
+
+            // помещаем состояние матрицы в стек матриц 
+            Gl.glPushMatrix();
+
+            // перемещаем камеру для более хорошего обзора объекта 
+            Gl.glTranslated(0, 0, -7);
+            // поворачиваем ее на 15 градусов 
+            Gl.glRotated(15, 1, 1, 0);
+
+            // помещаем состояние матрицы в стек матриц 
+            Gl.glPushMatrix();
+
+            // начинаем отрисовку объекта 
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+
+            // геометрические данные мы берем из массива GeomObject 
+            // рисуем основание с помощью зацикленной линии 
+            Gl.glVertex3d(GeomObject[0, 0], GeomObject[0, 1], GeomObject[0, 2]);
+            Gl.glVertex3d(GeomObject[1, 0], GeomObject[1, 1], GeomObject[1, 2]);
+            Gl.glVertex3d(GeomObject[2, 0], GeomObject[2, 1], GeomObject[2, 2]);
+
+            // завершаем отрисовку примитивов 
+            Gl.glEnd();
+
+            // рисуем линии от вершин основания к вершине пирамиды 
+            Gl.glBegin(Gl.GL_LINES);
+
+            Gl.glVertex3d(GeomObject[0, 0], GeomObject[0, 1], GeomObject[0, 2]);
+            Gl.glVertex3d(GeomObject[3, 0], GeomObject[3, 1], GeomObject[3, 2]);
+
+            Gl.glVertex3d(GeomObject[1, 0], GeomObject[1, 1], GeomObject[1, 2]);
+            Gl.glVertex3d(GeomObject[3, 0], GeomObject[3, 1], GeomObject[3, 2]);
+
+            Gl.glVertex3d(GeomObject[2, 0], GeomObject[2, 1], GeomObject[2, 2]);
+            Gl.glVertex3d(GeomObject[3, 0], GeomObject[3, 1], GeomObject[3, 2]);
+
+            // завершаем отрисовку примитивов 
+            Gl.glEnd();
+
+            // возвращаем состояние матрицы 
+            Gl.glPopMatrix();
+
+            // возвращаем состояние матрицы 
+            Gl.glPopMatrix();
+
+            // отрисовываем геометрию 
+            Gl.glFlush();
+
+            // обновляем состояние элемента 
+            AnT.Invalidate();*/
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // устанавливаем фокус в AnT 
+            AnT.Focus();
+        }
+
+        // функция масштабирования 
+        private void CreateZoom(float coef, int os)
+        {
+            // создаем матрицу 
+            float[,] Zoom3D = new float[3, 3]; Zoom3D[0, 0] = 1;
+            Zoom3D[1, 0] = 0;
+            Zoom3D[2, 0] = 0;
+
+            Zoom3D[0, 1] = 0;
+            Zoom3D[1, 1] = 1;
+            Zoom3D[2, 1] = 0;
+
+            Zoom3D[0, 2] = 0;
+            Zoom3D[1, 2] = 0;
+            Zoom3D[2, 2] = 1;
+
+            // устанавливаем коэффициент масштабирования для необходимой (выбранной и переданной в качестве параметра) оси 
+            Zoom3D[os, os] = coef;
+
+            // вызываем функцию для выполнения умножения матриц, представляющих собой координаты вершин геометрического объекта 
+            // на созданную в данной функции матрицу 
+            multiply(GeomObject, Zoom3D);
+        }
+
+        // перенос 
+        private void CreateTranslate(float translate, int os)
+        {
+            // в виду простоты данного алгоритма, мы упростили его обработку - 
+            // достаточно прибавить изменение (перенос) в координатах объекта по выбранной и переданной оси 
+            for (int ax = 0; ax < count_elements; ax++)
+            {
+                // обновление координат (для выбранной оси) 
+                GeomObject[ax, os] += translate;
+            }
+        }
+
+        // реализация поворота 
+        private void CreateRotate(float angle, int os)
+        {
+            // массив, который будет содержать матрицу 
+            float[,] Rotate3D = new float[3, 3];
+            // в зависимости от оси, матрицы будут кардинально различаться, 
+            // поэтому создаем необходимую матрицу в зависимости от оси, используя switch 
+            switch (os)
+            {
+                case 0: // вокруг оси Х 
+                    {
+                        Rotate3D[0, 0] = 1;
+                        Rotate3D[1, 0] = 0;
+                        Rotate3D[2, 0] = 0;
+
+                        Rotate3D[0, 1] = 0;
+                        Rotate3D[1, 1] = (float)Math.Cos(angle);
+                        Rotate3D[2, 1] = (float)-Math.Sin(angle);
+
+                        Rotate3D[0, 2] = 0;
+                        Rotate3D[1, 2] = (float)Math.Sin(angle);
+                        Rotate3D[2, 2] = (float)Math.Cos(angle);
+                        break;
+                    }
+                case 1: // вокруг оси Y 
+                    {
+                        Rotate3D[0, 0] = (float)Math.Cos(angle);
+                        Rotate3D[1, 0] = 0;
+                        Rotate3D[2, 0] = (float)Math.Sin(angle);
+                        Rotate3D[0, 1] = 0;
+                        Rotate3D[1, 1] = 1;
+                        Rotate3D[2, 1] = 0;
+                        Rotate3D[0, 2] = (float)-Math.Sin(angle);
+                        Rotate3D[1, 2] = 0;
+                        Rotate3D[2, 2] = (float)Math.Cos(angle);
+                        break;
+
+                    }
+                case 2: // вокруг оси Z 
+                    {
+                        Rotate3D[0, 0] = (float)Math.Cos(angle);
+                        Rotate3D[1, 0] = (float)-Math.Sin(angle);
+                        Rotate3D[2, 0] = 0;
+
+                        Rotate3D[0, 1] = (float)Math.Sin(angle);
+                        Rotate3D[1, 1] = (float)Math.Cos(angle);
+                        Rotate3D[2, 1] = 0;
+
+                        Rotate3D[0, 2] = 0;
+                        Rotate3D[1, 2] = 0;
+                        Rotate3D[2, 2] = 1;
+                        break;
+                    }
+
+            }
+
+            // вызываем функцию для выполнения умножения матриц, представляющих собой координаты вершин геометрического объекта 
+            // на созданную в данной функции матрицу 
+            multiply(GeomObject, Rotate3D);
+        }
+
+        // функция умножения матриц 
+        private void multiply(float[,] obj, float[,] matrix)
+        {
+            // временные переменные 
+            float res_1, res_2, res_3;
+
+            // проходим циклом по всем координатам (представляющие собой матрицу A [x,y,z]) 
+            // и умножаем каждую матрицу на матрицу B (переданную) 
+            // результат сразу заносим в массив геометрии 
+
+            for (int ax = 0; ax < count_elements; ax++)
+            {
+                res_1 = (obj[ax, 0] * matrix[0, 0] + obj[ax, 1] * matrix[0, 1] + obj[ax, 2] * matrix[0, 2]);
+                res_2 = (obj[ax, 0] * matrix[1, 0] + obj[ax, 1] * matrix[1, 1] + obj[ax, 2] * matrix[1, 2]);
+                res_3 = (obj[ax, 0] * matrix[2, 0] + obj[ax, 1] * matrix[2, 1] + obj[ax, 2] * matrix[2, 2]);
+
+                obj[ax, 0] = res_1;
+                obj[ax, 1] = res_2;
+                obj[ax, 2] = res_3;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
+    
 }
